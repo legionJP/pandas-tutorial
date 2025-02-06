ea937aa1891dfcff3e3bf557343ce321a44af8e7
JP PAL <83596339+legionJP@users.noreply.github.com>
2025-02-07 04:49:50 +0530

    Update series_dataframe.py

--- series_dataframe.py
+++ series_dataframe.py
@@ -192,3 +192,22 @@ print(df.mode())
 print(df.var())
 print(df.sum())
 print(df.corr())
+
+
+# Doing the union of the dataseries
+import pandas as pd
+import numpy as np
+df1 = pd.Series([2, 4, 5, 8, 10])
+df2 = pd.Series([8, 10, 13, 15, 17])
+p_union = pd.Series(np.union1d(df1, df2))  # union of series
+p_intersect = pd.Series(np.intersect1d(df1, df2))  # intersection of series
+unique_elements = p_union[~p_union.isin(p_intersect)]
+print(unique_elements)
+"""
+Output:
+0     2
+1     4
+2     5
+5    13
+6    15
+"""
